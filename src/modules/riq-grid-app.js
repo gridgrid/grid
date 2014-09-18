dff6a5a95a909c3da49c6585ebb0d2fd4027736b
Sterling Camden <sterling@relateiq.com>
2014-09-17 20:07:23 -0700

    start puttin together a visual test

--- src/modules/riq-grid-app.js
+++ src/modules/riq-grid-app.js
@@ -13,6 +13,32 @@ angular.module('riqGridApp', [])
             template: '<div>Grid App</div>',
             link: function ($scope, elem) {
 
+                var numRows = 1000;
+                var numCols = 100;
+                var grid = {};
+                grid.rowModel = require('@grid/row-model')(grid);
+                grid.colModel = require('@grid/col-model')(grid);
+                grid.dataModel = require('@grid/simple-data-model')(grid);
+                grid.cellScrollModel = require('@grid/cell-scroll-model')(grid);
+
+                if (numRows) {
+                    for (var r = 0; r < numRows; r++) {
+                        grid.rowModel.add({});
+                        if (numCols) {
+                            for (var c = 0; c < numCols || 0; c++) {
+                                if (r === 0) {
+                                    grid.colModel.add({});
+                                }
+                                grid.dataModel.set(r, c, {value: r + '-' + c});
+                            }
+                        }
+                    }
+                }
+
+                grid.viewModel = require('@grid/view-layer')(grid);
+                grid.viewModel.build(elem[0]);
+                grid.viewModel.draw();
+
             }
         };
     })
