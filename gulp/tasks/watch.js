511dcf26518e65aee5495076a95e7d070f150960
Sterling Camden <sterling@relateiq.com>
2014-11-20 12:04:07 -0800

    remove outdated

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -14,5 +14,4 @@ module.exports = gulp.task('watch', function () {
     watch({glob: [config.paths.src.index]}, ['index']);
     watch({glob: [config.paths.src.templates, config.paths.src.templatesHTML]}, ['templates']);
     watch({glob: [config.paths.src.stylesGlob]}, ['styles']);
-    //watch({glob: ['test-assets/bundle-tests.js']}, ['karma']);
 });
