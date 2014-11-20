5a6432c782ef78ff6162ce79eeb70b301cb47a16
Sterling Camden <sterling@relateiq.com>
2014-11-20 12:02:56 -0800

    remove unused and abstract better

--- gulp/tasks/defaultServe.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-var gulp = require('gulp');
-var runSequence = require('run-sequence');
-
-
-module.exports = gulp.task('defaultServe', ['default', 'serve']);
--- gulp/tasks/karma.js
+++ gulp/tasks/karma.js
@@ -1,7 +1,6 @@
 var gulp = require('gulp');
 var karma = require('gulp-karma');
 var argv = require('yargs').argv;
-var istanbul = require('browserify-istanbul');
 
 var karmaConf;
 require('../../karma.conf.js')({
@@ -9,31 +8,32 @@ require('../../karma.conf.js')({
         karmaConf = conf;
     }
 });
-gulp.task('karma', function () {
-    return gulp.src(karmaConf.files)
-        .pipe(karma({
-            configFile: 'karma.conf.js',
-            action: 'run'
-        }));
-});
 
-gulp.task('karma-watch', function () {
-    console.log('starting karma watch');
+function setupKarmaOpts(action) {
     var opts = {
         configFile: 'karma.conf.js',
-        action: 'watch'
+        action: action
     };
     opts.browserify = {
         cache: {}, packageCache: {}, fullPaths: true, debug: true
     };
     if (argv.coverage) {
-        opts.browserify.transform = [['browserify-istanbul', {
-            ignore: ['**/bower_components/**', '**/templates.js', '**/proto/**', '**/grid-spec-helper/**', '**/*.spec.js', '**/node_modules/!(@grid)/**', '**/src/modules/**'],
-            defaultIgnore: false
-        }]];
+        opts.browserify.transform = [
+            ['browserify-istanbul', {
+                ignore: ['**/bower_components/**', '**/templates.js', '**/proto/**', '**/grid-spec-helper/**', '**/*.spec.js', '**/node_modules/!(@grid)/**', '**/src/modules/**'],
+                defaultIgnore: false
+            }]
+        ];
 
     }
-
+    return opts;
+}
+gulp.task('karma-watch', function () {
     return gulp.src(karmaConf.files)
-        .pipe(karma(opts));
+        .pipe(karma(setupKarmaOpts('watch')));
+});
+
+gulp.task('karma', function () {
+    return gulp.src(karmaConf.files)
+        .pipe(karma(setupKarmaOpts('run')));
 });
