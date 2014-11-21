907747be7adaadae088997df3cf165bcca1bb81d
Sterling Camden <sterling@relateiq.com>
2014-11-20 20:43:50 -0800

    remove old globs

--- karma.conf.js
+++ karma.conf.js
@@ -1,9 +1,4 @@
 module.exports = function (config) {
-    var testsGlob = 'src/modules/**/*.spec.js';
-    var matchersGlob = 'src/modules/grid-spec-helper/matchers.js';
-    var preprocessors = {};
-    preprocessors[testsGlob] = ['browserify'];
-    preprocessors[matchersGlob] = ['browserify'];
     config.set({
 
         // base path that will be used to resolve all patterns (eg. files, exclude)
@@ -13,13 +8,9 @@ module.exports = function (config) {
 
         files: [
             'bower_components/angular/angular.js',
-            'bower_components/angular-mocks/angular-mocks.js',
-            matchersGlob,
-            testsGlob
+            'bower_components/angular-mocks/angular-mocks.js'
         ],
         exclude: [],
-
-        preprocessors: preprocessors,
 
         reporters: ['progress', /*'story',*/ 'coverage'],
 
