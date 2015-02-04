e0b5f4b833ee57ed97973cd9e4b0758a003a6d8b
Sterling Camden <sterling@relateiq.com>
2015-02-04 14:32:58 -0800

    formating

--- src/modules/add-dirty-props/index.js
+++ src/modules/add-dirty-props/index.js
@@ -1,18 +1,19 @@
-module.exports = function (obj, props, dirtyCleans) {
-    props.forEach(function (prop) {
+module.exports = function(obj, props, dirtyCleans) {
+    props.forEach(function(prop) {
         var val;
         var name = prop.name || prop;
         Object.defineProperty(obj, name, {
             enumerable: true,
-            get: function () {
+            get: function() {
                 return val;
-            }, set: function (_val) {
+            },
+            set: function(_val) {
                 var isChanged = _val !== val;
-                
+
                 val = _val;
 
                 if (isChanged) {
-                    dirtyCleans.forEach(function (dirtyClean) {
+                    dirtyCleans.forEach(function(dirtyClean) {
                         dirtyClean.setDirty();
                     });
                     if (prop.onDirty) {
