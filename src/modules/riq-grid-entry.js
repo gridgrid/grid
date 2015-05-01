c3f9ba3d2259e77ec02e459960fa61832262622c
Sterling Camden <sterling@relateiq.com>
2015-05-01 11:15:53 -0700

    add helpers for angular builders

--- src/modules/riq-grid-entry.js
+++ src/modules/riq-grid-entry.js
@@ -1,11 +1,10 @@
 module.exports =
     angular.module('riq-grid', [
-        require('./angular-decorator').name
+        require('./angular-decorator').name,
+        require('./angular-builder').name
     ])
-        .factory('RiqGridSrvc', function () {
-            return {
-                core: require('./core')
-            };
-        })
-;
-
+    .factory('RiqGridSrvc', function() {
+        return {
+            core: require('./core')
+        };
+    });
