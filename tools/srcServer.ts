a9ac0870ba3a36aa3da89822043c91480910f343
Joe Chrzanowski <joe@airkit.com>
2020-08-11 13:49:45 -0600

    feat(build): remove babel

--- tools/srcServer.ts
+++ tools/srcServer.ts
@@ -2,27 +2,26 @@
 // which supports hot reloading and synchronized testing.
 
 // Require Browsersync along with webpack and middleware for it
-import browserSync from 'browser-sync';
-import webpack from 'webpack';
-import webpackDevMiddleware from 'webpack-dev-middleware';
-import webpackHotMiddleware from 'webpack-hot-middleware';
-import createConfig from '../webpack/webpack.config';
+import browserSync from "browser-sync";
+import webpack from "webpack";
+import webpackDevMiddleware from "webpack-dev-middleware";
+import webpackHotMiddleware from "webpack-hot-middleware";
+import createConfig from "../webpack/webpack.config";
 
-const config = createConfig();
+const config: any = createConfig();
 const bundler = webpack(config);
 
 // Run Browsersync and use middleware for Hot Module Replacement
 browserSync({
   port: 8000,
   ui: {
-    port: 8001
+    port: 8001,
   },
   open: false,
   server: {
-    baseDir: 'src',
+    baseDir: "src",
 
     middleware: [
-
       webpackDevMiddleware(bundler, {
         // Dev middleware can't access config, so we provide publicPath
         publicPath: config.output.publicPath,
@@ -37,21 +36,20 @@ browserSync({
           hash: true,
           timings: true,
           chunks: true,
-          chunkModules: true
+          chunkModules: true,
         },
 
         // for other settings see
         // http://webpack.github.io/docs/webpack-dev-middleware.html
-      }),
+      } as any),
 
       // bundler should be the same as above
-      webpackHotMiddleware(bundler)
-    ]
+      webpackHotMiddleware(bundler),
+    ],
   },
 
   // no need to watch '*.js' here, webpack will take care of it for us,
   // including full page reloads if HMR won't work
-  files: [
-    'src/*.html'
-  ]
+  files: ["src/*.html"],
 });
+
