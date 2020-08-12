282583c04fbc93a223e31595c963d74d584aa102
Joe Chrzanowski <joe@airkit.com>
2020-08-12 10:05:32 -0600

    fix(build): add --no-sandbox

--- karma.conf.js
+++ karma.conf.js
@@ -1,36 +1,40 @@
-var webpackConfig = require("./webpack/webpack.config.js");
+var webpackConfig = require('./webpack/webpack.config.js');
 
 module.exports = function (config) {
   var configObject = {
-    frameworks: ["jasmine"],
-    basePath: "",
+    frameworks: ['jasmine'],
+    basePath: '',
     files: [
-      "node_modules/polyfill-function-prototype-bind/bind.js",
-      "node_modules/es6-promise/dist/es6-promise.js",
-      "src/modules/grid-spec-helper/matchers.js",
-      "webpack/test-entry.js",
+      'node_modules/polyfill-function-prototype-bind/bind.js',
+      'node_modules/es6-promise/dist/es6-promise.js',
+      'src/modules/grid-spec-helper/matchers.js',
+      'webpack/test-entry.js',
     ],
     preprocessors: {
-      "webpack/test-entry.js": ["webpack", "sourcemap"],
-      "src/modules/grid-spec-helper/matchers.js": ["webpack", "sourcemap"],
+      'webpack/test-entry.js': ['webpack', 'sourcemap'],
+      'src/modules/grid-spec-helper/matchers.js': ['webpack', 'sourcemap'],
     },
     webpackMiddleware: {
       noInfo: true,
     },
     autoWatch: true,
-    reporters: ["progress"],
-    browsers: ["ChromeHeadless"],
+    reporters: ['progress'],
+    browsers: ['ChromeHeadlessNoSandbox'],
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        flags: '--no-sandbox',
+      },
+    },
     junitReporter: {
-      outputFile: "test_out/unit.xml",
-      suite: "unit",
+      outputFile: 'test_out/unit.xml',
+      suite: 'unit',
     },
     notifyReporter: {
       reportSuccess: false,
     },
     browserNoActivityTimeout: 3000000,
     reportSlowerThan: 250,
-    webpack: require("./webpack/webpack.test"),
+    webpack: require('./webpack/webpack.test'),
   };
   config.set(configObject);
 };
-
